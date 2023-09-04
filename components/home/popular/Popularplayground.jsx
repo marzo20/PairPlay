bb0c6f4e4d48f233c446b9ced0d039eb4d95e24f
marzo20 <myjune91@gmail.com>
2023-09-04 01:15:30 -0700

    should find an api

--- components/home/popular/Popularplayground.jsx
+++ components/home/popular/Popularplayground.jsx
@@ -3,15 +3,40 @@ import { View, Text, TouchableOpacity, FlatList, ActivityIndicator } from 'react
 import { useRouter } from 'expo-router'
 import styles from './popularjobs.style'
 import { COLORS, SIZES } from '../../../constants'
-import PopularJobcard from '../../common/cards/popular/PopularJobCard'
+import PopularJobCard from '../../common/cards/popular/PopularJobCard'
 
 const Popularplayground = () => {
   const router = useRouter();
-
+  const isLoading = false;
+  const error = false;
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.headerTitle}>Popular Playground</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show All</Text>
+        </TouchableOpacity>
+      </View>
+      
+      <View style={styles.cardsContainer}> 
+        {isLoading ? (
+          <ActivityIndicator size="large" colors={COLORS.primary} />
+        ) : error ? (
+          <Text>Something went wrong</Text>
+        ) : (
+           <FlatList 
+              data={[1,2,3,4,5,6,7,8]}
+              renderItem={( {item} ) => (
+                <PopularJobCard 
+                  item={item}
+                />
+              )}
+              keyExtractor={item => item?.job_id}
+              contentContainerStyle={{ columnGap: SIZES.medium }}
+              horizontal
+           />
+        )
+      }
       </View>
       
     </View>
